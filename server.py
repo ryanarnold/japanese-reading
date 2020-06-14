39606d26c37f10711e6f805ea58df914aa84bf19
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2020-06-14 11:24:57 +0800

    Allow uploading of content

--- server.py
+++ server.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 from utils import retrieve_content_list
+import re
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     short_stories = retrieve_content_list('stories')
-    return render_template('index.html', short_stories=short_stories)
+    news_articles = retrieve_content_list('news')
+    light_novels = retrieve_content_list('ln')
+    return render_template('index.html', short_stories=short_stories, news_articles=news_articles, light_novels=light_novels)
 
 
 @app.route('/content/<subdir>/<content_id>')
@@ -21,3 +25,16 @@ def latest_log_file(subdir=None, content_id=None):
     print(content)
     
     return render_template('content.html', content_title=content_id, content=content)
+
+
+@app.route('/upload', methods=['POST', 'GET'])
+def upload():
+    if request.method == 'POST':
+        content_title = request.form['title']
+        content_type = request.form['type']
+        content = request.form['content']
+
+        with open('static\\content\\' + content_type + '\\' + content_title + '.txt', 'w', encoding='utf-8') as c:
+            c.write(content)
+
+    return render_template('upload.html')
